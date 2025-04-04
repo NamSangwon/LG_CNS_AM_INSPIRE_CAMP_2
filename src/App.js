f80e1f8fa783b8e033a0bdb0dcb1eb4087192c50
NamSangWon <nsw3265@naver.com>
2025-04-04 11:19:58 +0900

    Implement Rock Scissors Paper Game (Send Function to Child Component For Get Child Value)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import styles from './App.module.css';
+import {useState} from 'react'
 
 function Title() {
   return (
@@ -10,21 +11,27 @@ function Title() {
 }
 function Scissors(props) {
   return (
-    <div className={styles.control}>
+    <div className={styles.control} onClick={() => {
+      props.send(0);
+    }}>
       <img src='http://ggoreb.com/images/react/scissors.png' />
     </div>
   );
 }
 function Rock(props) {
   return (
-    <div className={styles.control}>
+    <div className={styles.control} onClick={() => {
+      props.send(1);
+    }}>
       <img src='http://ggoreb.com/images/react/rock.png' />
     </div>
   );
 }
 function Paper(props) {
   return (
-    <div className={styles.control}>
+    <div className={styles.control} onClick={() => {
+      props.send(2);
+    }}>
       <img src='http://ggoreb.com/images/react/paper.png' />
     </div>
   );
@@ -32,20 +39,54 @@ function Paper(props) {
 function Result(props) {
   return (
     <div className='result'>
-      <h1>Com:</h1>
-      <h1>Player:</h1>
-      <h1></h1>
+      <h1>Com:{props.result.com}</h1>
+      <h1>Player:{props.result.player}</h1>
+      <h1>{props.result.result}</h1>
     </div>
   );
 }
 function App() {
+  const [gameResult, setGameResult] = useState({});
+
+  // Such as Enum in C++
+  const RPS = {
+    0 : '가위',
+    1 : '바위',
+    2 : '보'
+  }
+
+  // 하위 컴포넌트에 '함수를 전송'하여 값을 가져오도록 함.
+  const send = (rps) => {
+    const com = parseInt(Math.random() * 3);
+
+    const result = "";
+    if ((rps  + 1) % 3 == com)
+      setGameResult({
+        com:RPS[com], 
+        player:RPS[rps], 
+        result:"COM WIN"
+      });
+    else if ((com + 1) % 3 == rps)
+      setGameResult({
+        com:RPS[com], 
+        player:RPS[rps], 
+        result:"PLAYER WIN"
+      });
+    else 
+      setGameResult({
+        com:RPS[com], 
+        player:RPS[rps], 
+        result:"DRAW"
+      });
+  }
+
   return (
     <div className="App">
       <Title />
-      <Scissors />
-      <Rock />
-      <Paper />
-      <Result />
+      <Scissors send={send}/>
+      <Rock send={send}/>
+      <Paper send={send}/>
+      <Result result={gameResult}/>
     </div>
   );
 }
