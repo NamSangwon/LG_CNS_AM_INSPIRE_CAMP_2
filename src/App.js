2ddb5cbe8b7ee1e3e2e12f2e93de8728cdac54ea
NamSangWon <nsw3265@naver.com>
2025-04-04 10:37:53 +0900

    Modify to Clean Code (Using by Object, useRef, Common Function)

--- src/App.js
+++ src/App.js
@@ -2,29 +2,55 @@ import { useRef, useState } from 'react';
 import './App.css';
 
 const App = () => {
-  // Convert Object for Simplify Data (생략)
+  const password_match = useRef();
   const [agree, setAgree] = useState(false);
-  const [id, setId] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const [email, setEmail] = useState("");
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const [address, setAddress] = useState("");
+  const [userInfo, setUserInfo] = useState({
+    userId : "",
+    password : "",
+    confirmPassword : "",
+    email : "",
+    phoneNumber : "",
+    address : ""
+  });
 
-  const agreeRegister = () => {
-    setAgree(!agree);
+  // 객체를 JSON 문자로 변환
+  // JSON.stringfy(userInfo);
+
+  // input 값에 대한 공용 함수
+  const handleChange = (event) => {
+    const name = event.target.name; // form input tag name
+    const value = event.target.value;
+
+    // 원래 값 가져온 후 name 키의 값을 업데이트
+    const newUserInfo = {...userInfo, [name] : value};
+    setUserInfo(newUserInfo); 
+
+    const pw = newUserInfo.password;
+    const cpw = newUserInfo.confirmPassword;
+
+    // style.display -> !important에 우선순위 밀림
+    if (pw == cpw)
+      // password_match.current.style.display = 'none';
+      password_match.current.classList.remove('pw-match');
+    else
+      // password_match.current.style.display = 'inline-block';
+      password_match.current.classList.add('pw-match');
   }
 
   const showInfo = () => {
     alert(`
       [입력된 내용]
-      아이디 : ${id}
-      비밀번호 : ${password}
-      이메일 : ${email}
-      전화번호 : ${phoneNumber}
-      주소 : ${address}
+      아이디 : ${userInfo.userId}
+      비밀번호 : ${userInfo.password}
+      이메일 : ${userInfo.email}
+      전화번호 : ${userInfo.phoneNumber}
+      주소 : ${userInfo.address}
     `);
   }
+
+  // [콜백 함수를 사용하는 방법이 더 좋음]
+  // setShow(!show) : 중간에 바뀐 show 값에 대해서 ! 적용
+  // setShow(prev => !prev) : 렌더링 시의 show 값에 대해서 ! 적용
 
   return (
     <div className="container">
@@ -34,7 +60,9 @@ const App = () => {
           <input
             type="checkbox"
             id="agree"
-            onChange={agreeRegister}
+            onChange={()=>{
+              setAgree(prev => !prev)
+            }}
           />
           <label className='agree' htmlFor="agree">이용약관에 모두 동의합니다</label>
         </div>
@@ -46,10 +74,9 @@ const App = () => {
                 <label htmlFor="userId">사용자 아이디</label>
                 <input
                   type="text"
-                  value = {id}
                   id="userId"
                   name="userId"
-                  onChange={(event)=>{setId(event.target.value)}}
+                  onChange={handleChange}
                   required
                 />
               </div>
@@ -58,10 +85,9 @@ const App = () => {
                 <label htmlFor="password">비밀번호</label>
                 <input
                   type="password"
-                  value = {password}
                   id="password"
                   name="password"
-                  onChange={(event)=>{setPassword(event.target.value)}}
+                  onChange={handleChange}
                   required
                 />
               </div>
@@ -70,23 +96,22 @@ const App = () => {
                 <label htmlFor="confirmPassword">비밀번호 확인</label>
                 <input
                   type="password"
-                  value = {confirmPassword}
                   id="confirmPassword"
                   name="confirmPassword"
-                  onChange={(event)=>{setConfirmPassword(event.target.value)}}
+                  onChange={handleChange}
                   required
                 />
-                {(password == confirmPassword) ? null : <label id="pw-match" className="pw-match">비밀번호가 일치해야 됩니다.</label>}
+                {/* {(password == confirmPassword) ? null : <label id="pw-match" className="pw-match">비밀번호가 일치해야 됩니다.</label>} */}
+                <label id="pw-match" className ref={password_match}>비밀번호가 일치해야 됩니다.</label>
               </div>
 
               <div className="form-group">
                 <label htmlFor="email">이메일</label>
                 <input
                   type="email"
-                  value = {email}
                   id="email"
                   name="email"
-                  onChange={(event)=>{setEmail(event.target.value)}}
+                  onChange={handleChange}
                   required
                 />
               </div>
@@ -95,10 +120,9 @@ const App = () => {
                 <label htmlFor="phoneNumber">전화번호</label>
                 <input
                   type="tel"
-                  value = {phoneNumber}
                   id="phoneNumber"
                   name="phoneNumber"
-                  onChange={(event)=>{setPhoneNumber(event.target.value)}}
+                  onChange={handleChange}
                   required
                 />
               </div>
@@ -107,10 +131,9 @@ const App = () => {
                 <label htmlFor="address">주소</label>
                 <input
                   type="text"
-                  value = {address}
                   id="address"
                   name="address"
-                  onChange={(event)=>{setAddress(event.target.value)}}
+                  onChange={handleChange}
                   required
                 />
               </div>
