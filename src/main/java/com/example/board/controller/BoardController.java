9af2a9f1bd95de0d8121898c13761a262b174112
NamSangwon <127469500+NamSangwon@users.noreply.github.com>
2025-04-30 17:04:30 +0900

    add controller (Login, Logout, Register & board - Search, Paging , list/view/update/delete)

--- src/main/java/com/example/board/controller/BoardController.java
+++ src/main/java/com/example/board/controller/BoardController.java
@@ -1,34 +1,164 @@
 package com.example.board.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.board.dto.BoardListDto;
+import com.example.board.dto.BoardViewDto;
+import com.example.board.entity.Board;
+import com.example.board.entity.User;
 import com.example.board.repository.BoardRepository;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class BoardController {
 	@Autowired
 	BoardRepository boardRepository;
 	
-	@GetMapping("/board/delete")
-	public String boardDelete() {
+	@GetMapping("/board/delete/{boardId}")
+	public String boardDelete(
+		@PathVariable Long boardId,
+		HttpSession session
+	) {
+		try {
+			Object obj = session.getAttribute("user_info");
+			if (obj == null)
+				throw new Exception();
+
+			if (boardId == null)
+				throw new Exception();
+
+			Optional<Board> opt = boardRepository.findById(boardId);
+			if (opt.isPresent()) {
+				Board board = opt.get();
+
+				if (obj.equals(board.getUser())) {
+					boardRepository.delete(board);
+				}
+			}
+
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e.getMessage());
+		}
+
 		return "redirect:/board";
 	}
 
-	@GetMapping("/board/update")
-	public String boardUpdate() {
+	@GetMapping("/board/update/{boardId}")
+	public String boardUpdate(
+		@PathVariable Long boardId,
+		HttpSession session,
+		Model model
+	) {
+		try {
+			Object obj = session.getAttribute("user_info");
+			
+			if (boardId == null)
+				throw new Exception();
+
+			if (obj == null) 
+				throw new Exception();
+
+			Optional<Board> opt = boardRepository.findById(boardId);
+			if (opt.isPresent())
+			{
+				Board board = opt.get();
+
+				if (obj.equals(board.getUser())) {
+					model.addAttribute("board", board);
+				}
+				else {
+					throw new Exception();
+				}
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e.getMessage());
+
+			return "redirect:/board";
+		}
 
 		return "board/update";
 	}
 		
-	@PostMapping("/board/update")
-	public String boardUpdatePost() {
+	@PostMapping("/board/update/{boardId}")
+	public String boardUpdatePost(
+		@PathVariable Long boardId,
+		@ModelAttribute Board board,
+		HttpSession session
+	) {
+		try {
+			Object obj = session.getAttribute("user_info");
+
+			if (obj == null)
+				throw new Exception();
+
+			if (obj instanceof User) {
+				User user = (User) obj;
+
+				board.setId(boardId);
+				board.setUser(user);
+
+				boardRepository.save(board);
+			}
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e.getMessage());
+		}
+
 		return "redirect:/board";
 	}
 
-	@GetMapping("/board/view")
-	public String boardView() {
+	@GetMapping("/board/view/{boardId}")
+	public String boardView(
+		@PathVariable Long boardId,
+		HttpSession session,
+		Model model
+	) {
+		try {
+			Object obj = session.getAttribute("user_info");
+
+			if (boardId == null)
+				throw new Exception();
+
+			Optional<Board> opt = boardRepository.findById(boardId);
+
+			if (opt.isPresent()) {
+				Board board = opt.get();
+
+				BoardViewDto boardViewDto = board.toBoardViewDto();
+
+				model.addAttribute("board", boardViewDto);
+					
+				if (obj != null && obj.equals(board.getUser())) {
+					model.addAttribute("owner", true);
+				}
+			}
+			
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e.getMessage());
+
+			return "redirect:/board";
+		}
+
 		return "board/view";
 	}
 
@@ -39,19 +169,75 @@ public class BoardController {
 	}
 
 	@GetMapping("/board/list")
-	public String boardList() {
+	public String boardList(
+		@RequestParam(defaultValue = "1") int page,
+		@RequestParam(defaultValue = "") String search,
+		Model model
+	) {
+		int pageCount = 10;
+
+		Pageable pageable = PageRequest.of(page - 1, pageCount);
+		Page<Board> pages = boardRepository.findByTitleContainingOrContentContaining(search, search, pageable);
+
+		List<BoardListDto> list = pages.stream().map(Board::toBoardListDto).toList();
+
+		int totalPages = pages.getTotalPages();
+		int startPage = (page - 1) / pageCount * pageCount + 1;
+		int endPage = (startPage + 9 > totalPages) ? (totalPages) : (startPage + 9);
+
+		model.addAttribute("search", search);
+
+		model.addAttribute("totalPages", totalPages);
+		model.addAttribute("page", page);
+		model.addAttribute("startPage", startPage);
+		model.addAttribute("endPage", endPage);
+
+		model.addAttribute("list", list);
 
 		return "board/list";
 	}
 
 	@GetMapping("/board/write")
-	public String boardWrite() {
+	public String boardWrite(HttpSession session) {
+		try {
+			Object obj = session.getAttribute("user_info");
+
+			if (obj == null) 
+				throw new Exception();
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e.getMessage());
+
+			return "redirect:/board";
+		}
 
 		return "board/write";
 	}
 	
 	@PostMapping("/board/write")
-	public String boardWritePost() {
+	public String boardWritePost(
+		@ModelAttribute Board board,
+		HttpSession session
+	) {
+		try {
+			Object obj = session.getAttribute("user_info");
+			
+			if (obj == null)
+				throw new Exception();
+
+			if (obj instanceof User) {
+				User user = (User) obj;
+
+				board.setUser(user);
+
+				// user의 PK인 id가 존재하지 않으면, 오류 발생
+				boardRepository.save(board);
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e.getMessage());
+		}
 
 		return "redirect:/board";
 	}
--- src/main/java/com/example/board/controller/UserController.java
+++ src/main/java/com/example/board/controller/UserController.java
@@ -1,23 +1,49 @@
 package com.example.board.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.board.entity.User;
+import com.example.board.repository.UserRepository;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class UserController {
+	@Autowired UserRepository userRepository;
+
 	@GetMapping("/signin")
 	public String signin() {
 		return "signin";
 	}
 	
 	@PostMapping("/signin")
-	public String signinPost() {
+	public String signinPost(
+		@ModelAttribute User user,
+		HttpSession session
+	) {
+		List<User> list = userRepository.findByEmailAndPwd(user.getEmail(), user.getPwd());
+		boolean isFindOnlyOne = (list.size() == 1);
+
+		if (isFindOnlyOne) {
+			session.setAttribute("user_info", list.get(0));
+		}
+		else {
+			return "redirect:/signin";
+		}
+
 		return "redirect:/";
 	}
 
 	@GetMapping("/signout")
-	public String signout() {
+	public String signout(HttpSession session) {
+		session.invalidate();
+
 		return "redirect:/";
 	}
 	
@@ -27,7 +53,20 @@ public class UserController {
 	}
 
 	@PostMapping("/signup")
-	public String signupPost() {
+	public String signupPost(
+		@ModelAttribute User user,
+		HttpSession session
+	) {
+		List<User> list = userRepository.findByEmailOrName(user.getEmail(), user.getName());
+		boolean isEmpty = list.isEmpty();
+
+		if (isEmpty) {
+			userRepository.save(user);
+		} 
+		else {
+			return "redirect:/signup";
+		}
+
 		return "redirect:/";
 	}
 }
