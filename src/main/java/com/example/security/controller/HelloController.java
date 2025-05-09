f92fd36f3b57b6f1caffda855e10302d9dc42059
NamSangwon <127469500+NamSangwon@users.noreply.github.com>
2025-05-09 15:47:52 +0900

    add MariaDB & JPA settings

--- src/main/java/com/example/security/controller/HelloController.java
+++ src/main/java/com/example/security/controller/HelloController.java
@@ -8,8 +8,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RestController
 public class HelloController {
     @GetMapping("/hello")
-    public String getMethodName() {
+    public String hello() {
         return "안녕하세요.";
+    }
+ 
+    @GetMapping("/admin")
+    public String admin() {
+        return "관리자 페이지 입니다.";
     }
     
 }
