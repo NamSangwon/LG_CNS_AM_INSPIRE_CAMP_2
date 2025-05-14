ba37c2e9f89b91924868bd39216153a74f0b1e2a
NamSangwon <127469500+NamSangwon@users.noreply.github.com>
2025-05-14 15:30:28 +0900

    modify Update Component

--- src/pages/Update.jsx
+++ src/pages/Update.jsx
@@ -1,28 +1,22 @@
 const Update = () => {
   return (
     <>
-      <div className="container mt-5">
-        <div className="card">
-          <div className="card-body">
-            <span className="badge bg-primary rounded-pill" th:text="${board.id} + '번'"></span>
-            <h5 className="card-title" th:text="${board.title}"></h5>
-            <h6 className="card-subtitle mb-2 text-muted" th:text="${board.userId}"></h6>
-            <p className="card-text" th:text="${board.content}"></p>
-          </div>
+      <div class="container mt-5">
+        <div class="row">
+          <form method="post" th:action="@{/board/update/} + ${board.id}">
+            <div class="mb-3">
+              <label for="title">Title:</label>
+              <input type="text" class="form-control" id="title" name="title" value="원본 값"/>
+            </div>
+            <div class="mb-3">
+              <label for="content">Content:</label>
+              <textarea class="form-control" rows="5" name="content" id="content" value="원본 값"></textarea>
+            </div>
+            <div class="d-grid gap-2">
+              <button class="btn btn-primary" id="complete">수정완료</button>
+            </div>
+          </form>
         </div>
-      </div>
-      <div className="container mt-5">
-        <ul className="nav justify-content-end">
-          <li className="nav-item">
-            <a className="nav-link" href="#" id="list">목록</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#" id="update">수정</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#" id="delete" th:num="${board.id}">삭제</a>
-          </li>
-        </ul>
       </div>
     </>
   );
