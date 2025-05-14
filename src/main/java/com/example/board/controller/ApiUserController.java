7b288482c86135609476b24cdc02f3bf18268e87
NamSangwon <127469500+NamSangwon@users.noreply.github.com>
2025-05-14 17:21:33 +0900

    modify to responsing by JSON Data

--- src/main/java/com/example/board/controller/ApiUserController.java
+++ src/main/java/com/example/board/controller/ApiUserController.java
@@ -1,8 +1,11 @@
 package com.example.board.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.ResponseEntity.BodyBuilder;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -22,19 +25,30 @@ public class ApiUserController {
 
 	@CrossOrigin
 	@PostMapping("/signup")
-	public BodyBuilder signupPost(
+	public ResponseEntity<?> signupPost(
 		@RequestBody User user
 	) {
 		List<User> list = userRepository.findByEmailOrName(user.getEmail(), user.getName());
 		boolean isEmpty = list.isEmpty();
 
+
+		Map<String, String> response = new HashMap<>();
+
 		if (isEmpty) {
 			userRepository.save(user);
-		} 
+
+			response.put("message", "회원 가입 실패");
+		}
 		else {
-			return ResponseEntity.badRequest();
+			response.put("message", "회원 가입 실패");
+
+			return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(response);
 		}
 
-		return ResponseEntity.ok();
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(response);
 	}
 }
